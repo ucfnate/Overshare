647d481e1ac63bafc33b5071a16fff02a14c7d51
Nate Robinson <ucfnate@me.com>
2025-08-31 19:16:17 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
+// app/page.js
 'use client';
 export const dynamic = 'force-dynamic';
-
+export const revalidate = 0;
 /* =========================================================
    Imports
 ========================================================= */
